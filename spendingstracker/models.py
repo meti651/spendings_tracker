4e3d65c4e836057e3133618c6c89452755be464b
meti651 <meti651@gmail.com>
2021-03-03 15:49:34 +0100

    spending class model

--- spendingstracker/models.py
+++ spendingstracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Spending(models.Model):
+    amount = models.IntegerField(default=1)
+    currency = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    date = models.DateTimeField()
+
+    def ___str___(self):
+        return f"{self.date} {self.currency}{self.amount}"
