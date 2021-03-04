84d162dd8b29b2fc0cc5f88a04d363cbef80ac5a
meti651 <meti651@gmail.com>
2021-03-04 20:57:43 +0100

    endpoints

--- spendingstracker/urls.py
+++ spendingstracker/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('/', views.index, name="index"),
+    re_path(r'filter/(?P<currency>\D+)', views.filter, name="filter"),
+    re_path(r'order/(?P<order_by>\D+)', views.order, name="order"),
+    path('<int:id>/delete', views.delete, name="delete"),
+    path('<int:id>/update', views.update, name="update"),
 ]
